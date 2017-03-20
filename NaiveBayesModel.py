157495e2af097423c069db03850a24d52b35533d
RafaelCartenet <rafael.cartenet@insa-rouen.fr>
2017-03-20 23:06:24 +0900

    correct vectorization + results

--- NaiveBayesModel.py
+++ NaiveBayesModel.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
 from bs4 import BeautifulSoup as bs
 from nltk.corpus import stopwords
 import re
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 
 # Naive Bayes Model for Sentimental Analysis
-#   loads imbd dataframe
-#   train Gaussian Nayve Bayes model
-#   predicts
 
 # FUNCTIONS
 def data_loading(filename):
@@ -18,73 +17,71 @@ def data_loading(filename):
     y_label = np.array(dataframe.ix[:,1])
     return x_data, y_label
 
-uselesswords = set(stopwords.words("english"))
+Stopwords = set(stopwords.words("english"))
 def ReviewsToWords(reviews):
     Words = []
     for i in range(len(reviews)):
         review = bs(reviews[i], "lxml").get_text()
         words = re.sub("[^a-zA-Z]", " ", review).lower().split()
-        words = [word for word in words if word not in uselesswords]
+        words = [word for word in words if word not in Stopwords]
         words = " ".join(words)
         Words.append(words)
     return Words
 
-def WordsToBOW(words):
+def BOWvectorizer(words):
     vectorizer = CountVectorizer(analyzer = "word",   \
                                  tokenizer = None,    \
                                  preprocessor = None, \
                                  stop_words = None,   \
                                  max_features = 3000)
-    BOWs = vectorizer.fit_transform(words)
-    return BOWs.toarray()
+    return vectorizer.fit(words)
 
-def results(pred, real):
-    TP = ((pred == "pos") & (real == "pos")).sum()
-    FP = ((pred == "pos") & (real == "neg")).sum()
-    FN = ((pred == "neg") & (real == "pos")).sum()
-    TN = ((pred == "neg") & (real == "neg")).sum()
-    recall = 100 * (TP / (float(TP) + FN))
-    precision = 100 * (TP / (float(TP) + FP))
-    F1score = 2*precision*recall / (precision + recall)
-    return recall, precision, F1score
+def train_nb(x, y, model = MultinomialNB):
+    model = model()
+    model.fit(x, y)
+    return model
+
+def classify_nb(model, x):
+    return model.predict(x)
 
 # DATA LOADING
 print "Loading Dataframe ..."
 datapath = "data/dataframe.pkl"
-reviews, sentiments = data_loading(datapath)
-ndata = reviews.size
+reviews, y = data_loading(datapath)
 print "DONE"
+
+# DATA SPLITTING
+ndata = reviews.size
+ndata_train = int(0.80*ndata)
+ndata_test = ndata - ndata_train
+reviews_train, y_train = reviews[:ndata_train], y[:ndata_train]
+reviews_test, y_test = reviews[-ndata_test:], y[-ndata_test:]
 
 # DATA PRE-PROCESSING
 print "Data Pre-Processing ..."
-Words = ReviewsToWords(reviews)
-BOWs = np.array(WordsToBOW(Words))
-ndata_train = int(0.90*ndata)
-ndata_test = ndata - ndata_train
-BOWs_train, sentiments_train = BOWs[:ndata_train], sentiments[:ndata_train]
-BOWs_test, sentiments_test = BOWs[-ndata_test:], sentiments[-ndata_test:]
+# Transforming Reviews into lists of words
+Words_train = ReviewsToWords(reviews_train)
+Words_test = ReviewsToWords(reviews_test)
+
+# Creating a Bag of Words vectorizer out of training data
+Vectorizer = BOWvectorizer(Words_train)
+
+# Vectorizing training and testing data
+x_train = Vectorizer.transform(Words_train).toarray()
+x_test = Vectorizer.transform(Words_test).toarray()
 print "DONE"
 
 # MODEL TRAINING
 print "Model Fitting ..."
-model = GaussianNB()
-model.fit(BOWs_train, sentiments_train)
+model = train_nb(x_train, y_train, MultinomialNB)
 print "DONE"
 print
 
 # PREDICTIONS
 print "Predictions on Train dataset :"
-y_pred = model.predict(BOWs_train)
-recall, precision, F1score = resultats(y_pred, sentiments_train)
-print "Recall    : %.3f" % recall, "%"
-print "Precision : %.3f" % precision, "%"
-print "F1score   : %.3f" % F1score, "%"
-print
+y_pred = classify_nb(model, x_train)
+print classification_report(y_train, y_pred)
 
 print "Predictions on Test dataset :"
-y_pred = model.predict(BOWs_test)
-recall, precision, F1score = resultats(y_pred, sentiments_test)
-print "Recall    : %.3f" % recall, "%"
-print "Precision : %.3f" % precision, "%"
-print "F1score   : %.3f" % F1score, "%"
-print
+y_pred = classify_nb(model, x_test)
+print classification_report(y_test, y_pred)
